6c7b54e823f7100f583fceaefcb6fb4178e0f3ea
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2023-03-05 15:14:54 +0200

    Update

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,24 +1,31 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
 import { useSelector } from 'react-redux';
 import { visibleContacts } from 'redux/selectors';
 
 import { ContactListItem } from 'components/ContactListItem/ContactListItem';
+import { ContactListIsEmpty } from 'components/ContactListIsEmpty/ContactListIsEmpty';
 
 import styles from './ContactList.module.scss';
 
 const ContactList = () => {
   const contacts = useSelector(visibleContacts);
-  console.log(contacts);
+  console.log(contacts.length);
+
+  // const el = window.document.getElementById('contactList');
+
+  // if (contacts.length > 1) {
+  //   el.classList.add('scrollbar');
+  // }
 
   return (
-    <div className={styles.contactListContaoner}>
-      <ul className={styles.contactList}>
-        {contacts.length === 0
-          ? Notify.failure('Contacts list is empty!')
-          : contacts.map(({ id, name, number }) => (
-              <ContactListItem key={id} id={id} name={name} number={number} />
-            ))}
+    <div className={styles.contactListContainer}>
+      <ul id="contactList" className={styles.contactList}>
+        {contacts.length === 0 ? (
+          <ContactListIsEmpty />
+        ) : (
+          contacts.map(({ id, name, number }) => (
+            <ContactListItem key={id} id={id} name={name} number={number} />
+          ))
+        )}
       </ul>
     </div>
   );
